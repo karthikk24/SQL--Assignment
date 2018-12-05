391e76ca466a252ad7646cbd19f7624c07cb1947
Karthik Kurpad <karthik.kurpad@gmail.com>
2018-12-05 00:16:51 -0600

    updated version

--- SQL_ASSIGNMENT.sql
+++ SQL_ASSIGNMENT.sql
@@ -166,6 +166,6 @@ GROUP BY name LIMIT 5;
 SELECT * FROM top_five_genres;
 
 # 8c. You find that you no longer need the view `top_five_genres`. Write a query to delete it.
-DROP VIEW top_five_grossing_genres;
+DROP VIEW top_five_genres;
  
  
